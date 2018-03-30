8c9858fe7f007b97c5477342bee0eec4449406d1
KaichingChang <kaichingc@gmail.com>
2018-03-30 17:00:42 +0800

    new

--- ArithmeticDemo/ViewController.swift
+++ ArithmeticDemo/ViewController.swift
@@ -1,15 +1,41 @@
 //
-//  ViewController.swift
-//  ArithmeticDemo
-//
-//  Created by 張凱慶 on 2018/3/27.
-//  Copyright © 2018年 kaiching. All rights reserved.
+//《程式語言教學誌》的範例程式
+// http://kaiching.org/
+// 檔名：range.swift
+// 功能：示範整數四則運算
+// 作者：張凱慶
 //
 
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var n1: NSTextField!
+    @IBOutlet weak var n2: NSTextField!
+    @IBOutlet weak var display: NSTextField!
+    @IBOutlet weak var op: NSComboBox!
+    
+    @IBAction func calculate(_ sender: NSButton) {
+        let a = Int(n1.stringValue)!
+        let b = Int(n2.stringValue)!
+        let c = op.indexOfSelectedItem
+        var result = 0
+        switch c {
+        case 0:
+            result = a + b
+        case 1:
+            result = a - b
+        case 2:
+            result = a * b
+        case 3:
+            result = a / b
+        default:
+            result = -9999
+        }
+        display.stringValue = String(result)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
